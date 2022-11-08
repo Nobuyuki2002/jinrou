b475f564b37d95f5e914443b8266c5f3409321d3
Nobuyuki2002 <e1b20066@oit.ac.jp>
2022-11-08 15:50:21 +0900

    first commit

---
+++ src/main/java/oit/is/ouchi/jinrou/JinrouApplication.java
@@ -0,0 +1,13 @@
+package oit.is.ouchi.jinrou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JinrouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JinrouApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/ouchi/jinrou/JinrouApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.ouchi.jinrou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JinrouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
