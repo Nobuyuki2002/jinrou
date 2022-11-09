117e45203536e29286bc720d574341e1cdf1476d
e1b20087 <e1b20087@oit.ac.jp>
2022-11-10 00:27:46 +0900

    rollesDBとroomDBの追加、h2-consoleへのリモートアクセスの対応

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,3 +5,19 @@ CREATE TABLE users (
     roles INT NOT NULL,
     isdeath boolean
 );
+
+CREATE TABLE rolles (
+    roll_id IDENTITY,
+    roll_name VARCHAR NOT NULL,
+    isVillager boolean
+);
+
+CREATE TABLE room (
+    room_id IDENTITY,
+    settng_id INT NOT NULL,
+    room_name VARCHAR NOT NULL,
+    room_pass INT NOT NULL,
+    roop_count INT NOT NULL,
+    winner boolean,
+    isActive boolean
+);
